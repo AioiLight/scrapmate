dcfa127c24d3c07c0e12e6317978950b57f6f32c
AioiLight <info@aioilight.space>
2020-10-28 00:39:04 +0900

    設定画面の日本語訳の追加

--- scrapmate/lib/generated/l10n.dart
+++ scrapmate/lib/generated/l10n.dart
@@ -35,7 +35,145 @@ class S {
     return Localizations.of<S>(context, S);
   }
 
+  /// `Settings`
+  String get settings {
+    return Intl.message(
+      'Settings',
+      name: 'settings',
+      desc: '',
+      args: [],
+    );
+  }
 
+  /// `Looks`
+  String get settings_looks {
+    return Intl.message(
+      'Looks',
+      name: 'settings_looks',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Theme`
+  String get settings_theme {
+    return Intl.message(
+      'Theme',
+      name: 'settings_theme',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `ScrapMate's theme`
+  String get settings_theme_desc {
+    return Intl.message(
+      'ScrapMate\'s theme',
+      name: 'settings_theme_desc',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Follow system`
+  String get settings_theme_default {
+    return Intl.message(
+      'Follow system',
+      name: 'settings_theme_default',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Light`
+  String get settings_theme_light {
+    return Intl.message(
+      'Light',
+      name: 'settings_theme_light',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Dark`
+  String get settings_theme_dark {
+    return Intl.message(
+      'Dark',
+      name: 'settings_theme_dark',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Black theme`
+  String get settings_black_theme {
+    return Intl.message(
+      'Black theme',
+      name: 'settings_black_theme',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Use pure black background color when using dark theme`
+  String get settings_black_theme_desc {
+    return Intl.message(
+      'Use pure black background color when using dark theme',
+      name: 'settings_black_theme_desc',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Scrapbox`
+  String get settings_scrapbox {
+    return Intl.message(
+      'Scrapbox',
+      name: 'settings_scrapbox',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Number of grid columns`
+  String get settings_grid {
+    return Intl.message(
+      'Number of grid columns',
+      name: 'settings_grid',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Number of grid columns in project page`
+  String get settings_grid_desc {
+    return Intl.message(
+      'Number of grid columns in project page',
+      name: 'settings_grid_desc',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Telomere`
+  String get settings_telomere {
+    return Intl.message(
+      'Telomere',
+      name: 'settings_telomere',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Show a telomere beside the paragraph`
+  String get settings_telomere_desc {
+    return Intl.message(
+      'Show a telomere beside the paragraph',
+      name: 'settings_telomere_desc',
+      desc: '',
+      args: [],
+    );
+  }
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {
@@ -44,6 +182,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'ja'),
     ];
   }
 
